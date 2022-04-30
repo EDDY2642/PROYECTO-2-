c613af9dbbf2891bf08424c5251568f4d18ba430
EDDY2642 <97562598+EDDY2642@users.noreply.github.com>
2022-04-29 20:58:12 -0500

    Add files via upload

---
+++ boleto.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilos2.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+	<title>CineItiz</title>
+    <link rel="stylesheet" href="css/boletos.css">
+</head>
+<body>
+
+
+<body>
+<header>
+		<div class="contenedor">
+			<h2 class="logotipo">CineItiz</h2>
+			<nav>
+			<a href="cliente.php" class="activo">Inicio</a>
+				<a href="quienes1.php">Quiénes somos</a>
+				<a href="ubicacion1.php">Ubicanos</a>
+				<a href="sanidad1.php">Medidas de Sanidad</a>
+				<a href="registro1.php">Cerrar sesión</a>
+			</nav>
+		</div>
+	</header>
+
+    <br>
+
+    <center><h1>PAGO APROBADO ✅</h1></center>
+	<br><br>
+<div class="i">
+<iframe src="boleto.pdf" width="100%" height="1000px" align="center" frameborder="0">
+</div>
+
+   
+   
+
+    
+</div>
+</body>
+</html>
